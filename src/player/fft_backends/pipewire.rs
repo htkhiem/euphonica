acf57ae9c4c55108fabcfc4190d03c02614177da
htkhiem <42716602+htkhiem@users.noreply.github.com>
2025-08-14 22:19:16 +0700

    Fix PipeWire receiver thread locking up

--- src/player/fft_backends/pipewire.rs
+++ src/player/fft_backends/pipewire.rs
@@ -103,6 +103,7 @@ impl FftBackendImpl for PipeWireFftBackend {
             self.pw_handle.borrow().is_none() && self.fft_handle.borrow().is_none()
         };
         if should_start {
+            // println!("Starting a new PipeWire listener...");
             let settings = settings_manager();
             let player_settings = settings.child("player");
             let n_samples = player_settings.uint("visualizer-fft-samples") as usize;
@@ -189,10 +190,13 @@ impl FftBackendImpl for PipeWireFftBackend {
 
                                 let (media_type, media_subtype) = match format_utils::parse_format(param) {
                                     Ok(v) => v,
-                                    Err(_) => return,
+                                    Err(_) => {
+                                        return
+                                    },
                                 };
 
                                 if media_type != MediaType::Audio || media_subtype != MediaSubtype::Raw {
+                                    println!("Not MediaType::Audio || MediaSubtype::Raw, skipping");
                                     return;
                                 }
 
@@ -200,6 +204,7 @@ impl FftBackendImpl for PipeWireFftBackend {
                                     .format
                                     .parse(param)
                                 else {
+                                    println!("Failed to parse format");
                                     let _ = fg_sender.send_blocking(FftStatus::Invalid);
                                     return;
                                 };
@@ -208,34 +213,43 @@ impl FftBackendImpl for PipeWireFftBackend {
                         .process(move |stream, user_data| match stream.dequeue_buffer() {
                             None => {return;},
                             Some(mut buffer) => {
-                                println!("Incoming data");
                                 let buffer_data = buffer.datas_mut();
                                 if buffer_data.is_empty() {
+                                    // println!("buffer_data is empty. Skipping");
                                     return;
                                 }
 
                                 let data = &mut buffer_data[0];
                                 let n_samples_avail = data.chunk().size() / (mem::size_of::<f32>() as u32);
                                 let n_channels = user_data.format.channels();
-                                if let Ok(mut format_lock) = pw_format.lock() {
-                                    *format_lock = AudioFormat {
-                                        rate: user_data.format.rate(),
-                                        chans: n_channels as u8,
-                                        bits: match user_data.format.format() {
-                                            SpaAudioFormat::F32BE | SpaAudioFormat::F32LE => 32,
-                                            _ => unimplemented!()
-                                            // Might support these directly in the future but for now we're only
-                                            // taking in float32le.
-                                            // SpaAudioFormat::F64BE | SpaAudioFormat::F64LE => 64,
-                                            // SpaAudioFormat::S16 | SpaAudioFormat::S16BE | SpaAudioFormat::S16LE | SpaAudioFormat::U16 | SpaAudioFormat::U16BE | SpaAudioFormat::U16LE => 16,
-                                            // SpaAudioFormat::S24 | SpaAudioFormat::S24BE | SpaAudioFormat::S24LE | SpaAudioFormat::U24 | SpaAudioFormat::U24BE | SpaAudioFormat::U24LE => 24,
-                                        }
-                                    };
+                                // println!("Locking pw_format...");
+                                {
+                                    if let Ok(mut format_lock) = pw_format.lock() {
+                                        // println!("Locked pw_format");
+                                        *format_lock = AudioFormat {
+                                            rate: user_data.format.rate(),
+                                            chans: n_channels as u8,
+                                            bits: match user_data.format.format() {
+                                                SpaAudioFormat::F32BE | SpaAudioFormat::F32LE => 32,
+                                                _ => unimplemented!()
+                                                // Might support these directly in the future but for now we're only
+                                                // taking in float32le.
+                                                // SpaAudioFormat::F64BE | SpaAudioFormat::F64LE => 64,
+                                                // SpaAudioFormat::S16 | SpaAudioFormat::S16BE | SpaAudioFormat::S16LE | SpaAudioFormat::U16 | SpaAudioFormat::U16BE | SpaAudioFormat::U16LE => 16,
+                                                // SpaAudioFormat::S24 | SpaAudioFormat::S24BE | SpaAudioFormat::S24LE | SpaAudioFormat::U24 | SpaAudioFormat::U24BE | SpaAudioFormat::U24LE => 24,
+                                            }
+                                        };
+                                    }
                                 }
+                                // println!("Unlocked pw_format");
+
                                 if let Some(samples) = data.data() {
+                                    // println!("Found samples");
                                     // ASSUME THERE ARE AT LEAST TWO CHANNELS.
                                     // I'm not gatekeeping but audiophiles listen to at least stereo sound :)
+                                    // println!("Locking buffer...");
                                     let mut locked_buffer = pw_samples.lock().unwrap();
+                                    // println!("Locked buffer");
                                     for n in (0..n_samples_avail).step_by(n_channels as usize) {
                                         let l_start = n as usize * mem::size_of::<f32>();
                                         let l_end = l_start + mem::size_of::<f32>();
@@ -246,6 +260,7 @@ impl FftBackendImpl for PipeWireFftBackend {
                                     }
                                     user_data.cursor_move = true;
                                 }
+                                // println!("Unlocked buffer");
                             }
                         })
                         .register() else {
@@ -284,6 +299,7 @@ impl FftBackendImpl for PipeWireFftBackend {
                             | pw::stream::StreamFlags::RT_PROCESS,
                         &mut params,
                     ) else {
+                        println!("Failed to connect PipeWire stream");
                         let _ = fg_sender.send_blocking(FftStatus::Invalid);
                         return;
                     };
@@ -304,81 +320,88 @@ impl FftBackendImpl for PipeWireFftBackend {
                     if stop_flag.load(Ordering::Relaxed) {
                         break 'outer;
                     }
-                    // Just get our own copy to minimise blocking
-                    let Ok(format_lock) = format.lock() else {
-                        let _ = fg_sender.send_blocking(FftStatus::Invalid);
-                        return;
-                    };
-                    // Skip processing until format is nonzero
-                    if format_lock.rate == 0 || format_lock.chans == 0 { continue; }
-                    // Copy ringbuffer to our static ones. Take care to read backward from the latest sample.
-                    if let Ok(ringbuffers) = samples.lock() {
-                        for pos in 0..n_samples {
-                            fft_buf_left[n_samples - pos - 1] = *ringbuffers.0.get_signed(-1 - pos as isize).unwrap_or(&(0.0 as f32));
-                            fft_buf_right[n_samples - pos - 1] = *ringbuffers.1.get_signed(-1 - pos as isize).unwrap_or(&(0.0 as f32));
-                        }
-                    }
-                    // These should be applied on-the-fly
-                    let bin_mode =
-                        if player_settings.boolean("visualizer-spectrum-use-log-bins") {
-                            super::fft::BinMode::Logarithmic
-                        } else {
-                            super::fft::BinMode::Linear
+                    // println!("FFT: locking format");
+                    {
+                        let Ok(format_lock) = format.lock() else {
+                            println!("PipeWire FFT: unable to lock format");
+                            let _ = fg_sender.send_blocking(FftStatus::Invalid);
+                            return;
                         };
-                    let fps = player_settings.uint("visualizer-fps") as f32;
-                    let min_freq =
-                        player_settings.uint("visualizer-spectrum-min-hz") as f32;
-                    let max_freq =
-                        player_settings.uint("visualizer-spectrum-max-hz") as f32;
-                    let curr_step_weight = player_settings
-                        .double("visualizer-spectrum-curr-step-weight")
-                        as f32;
-                    // Compute outside of output mutex lock please
-
-                    super::fft::get_magnitudes(
-                        &format_lock,
-                        &mut fft_buf_left,
-                        &mut curr_step_left,
-                        n_bins as u32,
-                        bin_mode,
-                        min_freq,
-                        max_freq,
-                    );
-                    super::fft::get_magnitudes(
-                        &format_lock,
-                        &mut fft_buf_right,
-                        &mut curr_step_right,
-                        n_bins as u32,
-                        bin_mode,
-                        min_freq,
-                        max_freq,
-                    );
-                    // Replace last frame
-                    if let Ok(mut output_lock) = output.lock() {
-                        if output_lock.0.len() != n_bins
-                            || output_lock.1.len() != n_bins
-                        {
-                            output_lock.0.clear();
-                            output_lock.1.clear();
-                            for _ in 0..n_bins {
-                                output_lock.0.push(0.0);
-                                output_lock.1.push(0.0);
+                        // Skip processing until format is nonzero
+                        if format_lock.rate == 0 || format_lock.chans == 0 { continue; }
+                        // Copy ringbuffer to our static ones. Take care to read backward from the latest sample.
+                        if let Ok(ringbuffers) = samples.lock() {
+                            for pos in 0..n_samples {
+                                fft_buf_left[n_samples - pos - 1] = *ringbuffers.0.get_signed(-1 - pos as isize).unwrap_or(&(0.0 as f32));
+                                fft_buf_right[n_samples - pos - 1] = *ringbuffers.1.get_signed(-1 - pos as isize).unwrap_or(&(0.0 as f32));
                             }
                         }
-                        for i in 0..n_bins {
-                            // FIXME: To line up with FIFO backend we should scale this backend's magnitudes
-                            // up by 5x.
-                            output_lock.0[i] = curr_step_left[i] * curr_step_weight * 5.0
-                                + output_lock.0[i] * (1.0 - curr_step_weight);
-                            output_lock.1[i] = curr_step_right[i]
-                                * curr_step_weight * 5.0
-                                + output_lock.1[i] * (1.0 - curr_step_weight);
+                        // These should be applied on-the-fly
+                        let bin_mode =
+                            if player_settings.boolean("visualizer-spectrum-use-log-bins") {
+                                super::fft::BinMode::Logarithmic
+                            } else {
+                                super::fft::BinMode::Linear
+                            };
+                        let min_freq =
+                            player_settings.uint("visualizer-spectrum-min-hz") as f32;
+                        let max_freq =
+                            player_settings.uint("visualizer-spectrum-max-hz") as f32;
+                        let curr_step_weight = player_settings
+                            .double("visualizer-spectrum-curr-step-weight")
+                            as f32;
+                        // Compute outside of output mutex lock please
+
+                        super::fft::get_magnitudes(
+                            &format_lock,
+                            &mut fft_buf_left,
+                            &mut curr_step_left,
+                            n_bins as u32,
+                            bin_mode,
+                            min_freq,
+                            max_freq,
+                        );
+                        super::fft::get_magnitudes(
+                            &format_lock,
+                            &mut fft_buf_right,
+                            &mut curr_step_right,
+                            n_bins as u32,
+                            bin_mode,
+                            min_freq,
+                            max_freq,
+                        );
+                        // Replace last frame
+                        // println!("FFT: Locking output");
+                        if let Ok(mut output_lock) = output.lock() {
+                            // println!("Locked output");
+                            if output_lock.0.len() != n_bins
+                                || output_lock.1.len() != n_bins
+                            {
+                                output_lock.0.clear();
+                                output_lock.1.clear();
+                                for _ in 0..n_bins {
+                                    output_lock.0.push(0.0);
+                                    output_lock.1.push(0.0);
+                                }
+                            }
+                            for i in 0..n_bins {
+                                // FIXME: To line up with FIFO backend we should scale this backend's magnitudes
+                                // up by 5x.
+                                output_lock.0[i] = curr_step_left[i] * curr_step_weight * 5.0
+                                    + output_lock.0[i] * (1.0 - curr_step_weight);
+                                output_lock.1[i] = curr_step_right[i]
+                                    * curr_step_weight * 5.0
+                                    + output_lock.1[i] * (1.0 - curr_step_weight);
+                            }
+                            // println!("FFT L: {:?}\tR: {:?}", &output_lock.0, &output_lock.1);
+                        } else {
+                            println!("FFT: Failed to lock output for writing");
+                            let _ = fg_sender.send_blocking(FftStatus::Invalid);
+                            return;
                         }
-                        // println!("FFT L: {:?}\tR: {:?}", &output_lock.0, &output_lock.1);
-                    } else {
-                         let _ = fg_sender.send_blocking(FftStatus::Invalid);
-                        return;
                     }
+                    // println!("FFT: Unlocked output & format. Now sleeping...");
+                    let fps = player_settings.uint("visualizer-fps") as f32;
                     thread::sleep(Duration::from_millis((1000.0 / fps).floor() as u64));
                 }
             });
@@ -413,6 +436,7 @@ impl FftBackendImpl for PipeWireFftBackend {
         let fft_stop = self.stop_flag.clone();
         fft_stop.store(true, Ordering::Relaxed);
         if let Some(sender) = self.pw_sender.take() {
+            println!("Stopping PipeWire thread...");
             if sender.send(Terminate).is_ok() {
                 if let Some(handle) = self.pw_handle.take() {
                     let _ = glib::MainContext::default().block_on(handle);
