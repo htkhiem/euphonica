7daf778b2a291c883d118dd35345f94321f0d9f1
htkhiem2000 <42716602+htkhiem@users.noreply.github.com>
2026-04-25 09:47:40 +0700

    Tweak seekbar size & gradient

--- src/player/seekbar2.rs
+++ src/player/seekbar2.rs
@@ -220,8 +220,8 @@ mod imp {
                 // In dark mode, the seekbar highlight glows the accent colour and the cursor glows white.
                 [
                     gsk::ColorStop::new(0.0, bottom),
-                    gsk::ColorStop::new(0.15, accent.with_alpha(0.7)),
-                    gsk::ColorStop::new(0.3, accent.with_alpha(0.4)),
+                    gsk::ColorStop::new(0.15, accent.with_alpha(0.5)),
+                    gsk::ColorStop::new(0.3, accent.with_alpha(0.3)),
                     gsk::ColorStop::new(0.75, accent.with_alpha(0.0)),
                 ]
             } else {
@@ -260,11 +260,13 @@ mod imp {
                 &[
                     gsk::ColorStop::new(0.0, cursor_color.with_alpha(0.0)),
                     gsk::ColorStop::new(0.125, cursor_color.with_alpha(0.1)),
-                    gsk::ColorStop::new(0.25, cursor_color.with_alpha(0.4)),
+                    gsk::ColorStop::new(0.245, cursor_color.with_alpha(0.4)),
+                    gsk::ColorStop::new(0.255, cursor_color.with_alpha(0.4)),
                     gsk::ColorStop::new(0.375, cursor_color.with_alpha(0.1)),
                     gsk::ColorStop::new(0.5, cursor_color.with_alpha(0.0)),
                     gsk::ColorStop::new(0.625, cursor_color.with_alpha(0.1)),
-                    gsk::ColorStop::new(0.75, cursor_color.with_alpha(0.4)),
+                    gsk::ColorStop::new(0.745, cursor_color.with_alpha(0.4)),
+                    gsk::ColorStop::new(0.755, cursor_color.with_alpha(0.4)),
                     gsk::ColorStop::new(0.875, cursor_color.with_alpha(0.1)),
                     gsk::ColorStop::new(1.0, cursor_color.with_alpha(0.0)),
                 ],
